60db3bc9a89fed2feb1b319e41dbd1b6cb719f33
Prasenjit Aluni <prasenjit.aluni@codeclouds.com>
2024-01-15 16:31:50 +0530

    Mysql DB setup and fetch data

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ Using SQL with Sequelize  <br />
 ## Important command list
 
 npm init <br />
-npm i --save express express-session body-parser ejs mongodb mongoose connect-mongodb-session bcryptjs cookie-parser csurf csrf <br />
+npm i --save express express-session body-parser ejs jsonwebtoken bcryptjs cookie-parser csurf csrf <br />
+npm i --save mongodb mongoose connect-mongodb-session mysql2 sequelize <br />
+
 npm i --save-dev nodemon <br />
 nodemon app.js <br />
 
--- controllers/article.js
+++ controllers/article.js
@@ -5,7 +5,7 @@ exports.getArticleList = async (req, res, next) => {
 
     let fetchResult  = await article.fetchAll();
     let fetchData    = await fetchResult[0];
-    return res.render('./article/list', {data:fetchData});
+    return res.render('./article/list', {type:'Asyn Method', data:fetchData});
 }
 
 
@@ -14,7 +14,7 @@ exports.getArticleListing = (req, res, next) => {
 
     return article.fetchAll()
                     .then(([rows, fieldData])=>{
-                        res.render('./article/list', {data:rows});
+                        res.render('./article/list', {type:'Asyn with then, catch ',data:rows});
                     })
                     .catch(err=>console.log(err));
 
