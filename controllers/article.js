1eb0b528e1b46df5c8385c98b8126d9028459aa4
Prasenjit Aluni <prasenjit.aluni@codeclouds.com>
2024-01-16 13:58:00 +0530

    Request query param

--- controllers/article.js
+++ controllers/article.js
@@ -5,7 +5,9 @@ exports.getArticleList = async (req, res, next) => {
 
     let fetchResult  = await article.fetchAll();
     let fetchData    = await fetchResult[0];
-    return res.render('./article/list', {pageTitle:'Article list using Asyn method with Mysql', data:fetchData});
+    const pageNo     = req.query.page ?? 0;
+
+    return res.render('./article/list', {pageNo : pageNo, data:fetchData, pageTitle:'Article list using Asyn method with Mysql'});
 }
 
 
@@ -14,7 +16,7 @@ exports.getArticleListing = (req, res, next) => {
 
     return article.fetchAll()
                     .then(([rows, fieldData])=>{
-                        res.render('./article/list', {pageTitle:'Article list using Asyn with then, catch with Mysql',data:rows});
+                        res.render('./article/list', {pageNo : pageNo, data:rows, pageTitle:'Article list using Asyn with then, catch with Mysql'});
                     })
                     .catch(err=>console.log(err));
 
--- controllers/item.js
+++ controllers/item.js
@@ -3,9 +3,11 @@ const Item      = require('../models/item');
 
 exports.getItemList = (req, res, next) => {
 
+    const pageNo = req.query.page ?? 0;
+
     Item.findAll()
         .then(fetchData=>{
-            return res.render('./article/list', {pageTitle:'Item list using Asyn method with Mysql Sequelize.', data:fetchData});
+            return res.render('./article/list', {pageNo : pageNo, data:fetchData, pageTitle:'Item list using Asyn method with Mysql Sequelize.'});
         })
         .catch(err=>console.log(err));
 
